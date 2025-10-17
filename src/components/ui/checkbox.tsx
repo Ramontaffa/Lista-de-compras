7b4fc1558608c7f50a7ce479987501418dda36b1
Ramontaffa <taffarelramon1@gmail.com>
2025-10-17 15:56:26 -0300

    feat(checkbox): update styles for Checkbox component to enhance visual consistency and accessibility

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -14,7 +14,10 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer size-4 shrink-0 rounded-[4px] border border-purple-light bg-gray-500 shadow-xs hover:bg-purple-dark transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "text-white data-[state=checked]:bg-success data-[state=checked]:border-none data-[state=checked]:text-white data-[state=checked]:hover:bg-success-light",
+        "focus-visible:border-purple-light focus-visible:ring-purple-500/20 focus-visible:ring-[3px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
       {...props}
