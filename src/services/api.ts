75f427b49ac653dc97010ac8844c6a2258dad750
Ramontaffa <taffarelramon1@gmail.com>
2025-10-19 15:25:21 -0300

    fix(api): update item endpoints to remove leading slashes for consistency

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,41 @@ const api = axios.create({
   baseURL: 'https://lista-de-compras-api.onrender.com/',
 });
 
+// Log request
+api.interceptors.request.use(request => {
+  console.log('Request:', {
+    url: request.url,
+    method: request.method,
+    data: request.data,
+    headers: request.headers,
+  });
+  return request;
+});
+
+// Log response
+api.interceptors.response.use(
+  response => {
+    console.log('Response:', {
+      url: response.config.url,
+      status: response.status,
+      data: response.data,
+      headers: response.headers,
+    });
+    return response;
+  },
+  error => {
+    if (error.response) {
+      console.log('Response Error:', {
+        url: error.response.config.url,
+        status: error.response.status,
+        data: error.response.data,
+        headers: error.response.headers,
+      });
+    } else {
+      console.log('Request Error:', error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
--- src/services/items.ts
+++ src/services/items.ts
@@ -2,21 +2,21 @@ import api from "./api";
 import { ShoppingItemProps } from "../types/itemProps";
 
 export const getItems = async () => {
-  const response = await api.get("/items");
+  const response = await api.get("items");
   return response.data;
 };
 
 export const addItem = async (item: Omit<ShoppingItemProps, "id">) => {
-  const response = await api.post("/items", item);
+  const response = await api.post("items", item);
   return response.data;
 };
 
 export const deleteItem = async (id: string) => {
-  const response = await api.delete(`/items/${id}`);
+  const response = await api.delete(`items/${id}`);
   return response.data;
 };
 
 export const updateItem = async (id: string, item: Partial<ShoppingItemProps>) => {
-  const response = await api.put(`/items/${id}`, item);
+  const response = await api.put(`items/${id}`, item);
   return response.data;
 };
