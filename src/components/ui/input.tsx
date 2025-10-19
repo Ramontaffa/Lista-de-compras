780760928a74105fba00cac2dc8dc23b2e3faaac
Ramontaffa <taffarelramon1@gmail.com>
2025-10-19 16:08:23 -0300

    fix(Input): update focus styles for improved accessibility and visual clarity

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       className={cn(
         "file:text-white file:bg-gray-500 text-white placeholder:text-white selection:text-white selection:bg-transparent dark:bg-input/30 w-full min-w-0 rounded-md border border-gray-300 bg-gray-500 px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus:border-[1px] focus:border-purple-light focus-visible:border-purple-light focus-visible:ring-purple-500/20 focus-visible:ring-[3px] focus:bg-gray-500 focus-visible:bg-gray-500",
+        "focus:border-[2px] focus:border-purple-light focus-visible:border-purple-light",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
